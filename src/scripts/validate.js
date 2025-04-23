b20417a3ad8d87418703622425bf95157e63185e
Relax <nikitafon2005@gmail.com>
2025-04-23 12:21:42 +0300

    add new validate

--- src/scripts/validate.js
+++ src/scripts/validate.js
@@ -1,54 +1,72 @@
 /** Функция валидации всех форм */
 export const enableValidation = (config) => {
+  // Получаем массив всех форм, используя переданный селектор формы из конфигурации
   const formList = Array.from(document.querySelectorAll(config.formSelector));
 
+  // Перебираем каждую форму в списке
   formList.forEach((form) => {
+    // Добавляем обработчик события для отключения стандартного поведения отправки формы
     form.addEventListener('submit', disableSubmit);
-    form.addEventListener('input', () => {
-      toggleButton(form, config);
-    });
-
-    addInputListners(form, config);
+    
+    // Добавляем обработчик события input для каждой формы для переключения состояния кнопки отправки
+    form.addEventListener('input', () => toggleButton(form, config));
+    
+    // Добавляем слушатели событий для всех инпутов формы
+    addInputListeners(form, config);
+    
+    // Переключаем состояние кнопки при загрузке страницы
     toggleButton(form, config);
   });
 };
 
 /** Функция отключения перезагрузки */
 const disableSubmit = (evt) => {
+  // Отменяем стандартное поведение отправки формы
   evt.preventDefault();
-}
+};
 
 /** Функция вывода сообщения валидации */
 const handleFormInput = (evt, config) => {
+  // Получаем элемент, вызвавший событие
   const input = evt.target;
-  const inputId = input.id;
-  const errorElement = document.querySelector(`#${inputId}-error`);
+  
+  // Находим элемент ошибки, связанный с этим инпутом по его ID
+  const errorElement = document.querySelector(`#${input.id}-error`);
 
+  // Проверяем валидность инпута
   if (input.validity.valid) {
+    // Если инпут валиден, удаляем класс ошибки и очищаем сообщение об ошибке
     input.classList.remove(config.inputErrorClass);
     errorElement.textContent = '';
   } else {
+    // Если инпут не валиден, добавляем класс ошибки и отображаем сообщение валидации
     input.classList.add(config.inputErrorClass);
     errorElement.textContent = input.validationMessage;
   }
-}
+};
 
 /** Функция переключения кнопки submit */
 const toggleButton = (form, config) => {
-  const buttonSubmint = form.querySelector(config.submitButtonSelector);
+  // Находим кнопку отправки внутри формы по селектору из конфигурации
+  const buttonSubmit = form.querySelector(config.submitButtonSelector);
+  
+  // Проверяем валидность всей формы
   const isFormValid = form.checkValidity();
+  
+  // Блокируем или разблокируем кнопку в зависимости от валидности формы
+  buttonSubmit.disabled = !isFormValid;
+  
+  // Добавляем или удаляем класс отключенной кнопки
+  buttonSubmit.classList.toggle('popup__button_disabled', !isFormValid);
+};
 
-  buttonSubmint.disabled = !isFormValid;
-  buttonSubmint.classList.toggle('popup__button_disabled', !isFormValid);
-}
-
-/** Объявляю функцию слушателей всех инпутов */
-const addInputListners = (form, config) => {
+/** Функция добавления слушателей ко всем инпутам */
+const addInputListeners = (form, config) => {
+  // Получаем массив всех инпутов формы по селектору из конфигурации
   const inputList = Array.from(form.querySelectorAll(config.inputSelector));
 
-  inputList.forEach(function (item) {
-    item.addEventListener('input', (evt) => {
-      handleFormInput(evt, config);
-    })
-  });
-}
+  // Перебираем каждый инпут и добавляем обработчик события input
+  inputList.forEach((input) =>
+    input.addEventListener('input', (evt) => handleFormInput(evt, config))
+  );
+};
